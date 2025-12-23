fd929b4a46b7fde2859f3b17c3c56d8015721819
davidyeat <david.yeat123@gmail.com>
2025-12-23 20:23:31 +0700

    week-10

--- START CODE (W10)/EXERCISE 3/js/app.js
+++ START CODE (W10)/EXERCISE 3/js/app.js
@@ -14,18 +14,35 @@ function randomColor() {
 function createCard() {
  
   // 1 - Random color for card
-   
+  let cardColor = randomColor();
    
   // 2 - Set card text
+  const cardText = "Hello";
   
   // 3 - Set card footer
-   
+  const cardFooter = document.createElement('div');
+  cardFooter.className = 'card-footer';
 
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.style.backgroundColor = cardColor;
+  
   //  4 - Manage footer button
-   
+  const button = document.createElement('button');
+  button.textContent = 'Remove Card';
+  button.addEventListener("click", function () {
+    card.remove();
+  });
 
   // 5 - Add card to containers
- 
+  const container = document.querySelector('.container');
+  const p = document.createElement('p');
+  p.textContent = cardText;
+
+  cardFooter.appendChild(button);
+  card.appendChild(p);
+  card.appendChild(cardFooter);
+  container.appendChild(card);
 }
 
 
