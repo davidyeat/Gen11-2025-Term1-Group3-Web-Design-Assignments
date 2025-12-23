94944dc5c77f5472db0fbc85198f2039435338e5
davidyeat <david.yeat123@gmail.com>
2025-12-23 23:50:20 +0700

    init

--- START CODE (W10)/EXERCISE 4/index.js
+++ START CODE (W10)/EXERCISE 4/index.js
@@ -29,21 +29,75 @@ function show(element) {
 
 function showGame() {
   // You can use this function to dispaly the Game view
+  if(localStorage.getItem("lost") === "true"){
+    showLost();
+    return;
+  }
+
+  hide(lostView);
+  hide(wonView);
+  show(passwordView);
+  passwordInput.value = "";
+  passwordInput.focus();
 }
 
 function showWin() {
   // You can use this function to dispaly the Win View
+  hide(passwordView);
+  show(wonView);
 }
 function showLost() {
-    // You can use this function to dispaly the Lost View
- 
+  // You can use this function to dispaly the Lost View
+  hide(passwordView);
+  show(lostView);
 }
 
- 
 function handleCheck() {
-     // Manage your logic when the button is pressed
+  // Manage your logic when the button is pressed
+  if(parseInt(passwordInput.value) === SECRET_CODE){
+    localStorage.setItem("won", "true");
+    showWin();
+  } else{
+    chanceRemaining--;
+    localStorage.setItem("attempts", chanceRemaining);
+
+    instructionLabel.textContent = `Wrong password! You have ${chanceRemaining} attempts left.`;
+    passwordInput.value = "";
+    passwordInput.focus();
+
+    if(chanceRemaining === 0){
+      localStorage.setItem("lost", "true");
+      showLost();
+    }
+  }
+}
+
+function handleEnterPasserord(event){
+  if(event.key === "Enter"){
+    handleCheck();
+  }
 }
 
 // MAIN   ---------------------------------------------------------
+let chanceRemaining = localStorage.getItem("attempts");
 
- 
+if(chanceRemaining === null){
+  chanceRemaining = 3;
+  localStorage.setItem("attempts", chanceRemaining);
+} else {
+  chanceRemaining = parseInt(chanceRemaining);
+}
+
+const won = localStorage.getItem("won");
+const lost = localStorage.getItem("lost");
+
+if(won === "true"){
+  showWin();
+}
+else if(lost === "true") {
+  showLost();
+}
+else {
+  showGame();
+}
+
--- START CODE (W10)/EXERCISE 5/js/script.js
+++ START CODE (W10)/EXERCISE 5/js/script.js
@@ -1,28 +1,77 @@
 // ----------------------------------------------------------------------------
+// MAIN
+// ----------------------------------------------------------------------------
+
+let taskList = [];
+
+// ----------------------------------------------------------------------------
 // FUNCTIONS
 // ----------------------------------------------------------------------------
+
+// const { createElement } = require("react");
 
 function addItem() {
   // 1- Create a new task
   // TODO
+  const task = {
+    description: "",
+    priority: 0
+  };
 
   //  2- Set the description from the text field
   // TODO
+  const descriptionInput = document.getElementById("description");
+  const descriptionValue = descriptionInput.value.trim();
+  task.description = descriptionValue;
 
   // 3- Set the priority from select field
   // TODO
+  const prioritySelect = document.getElementById("priority");
+  const priorityValue = parseInt(prioritySelect.value);
+  task.priority = priorityValue;
 
   // 4- Add the new object to the array
   // TODO
-
-  console.log(tasks);
+  taskList.push(task);
+  displayTasks(taskList);
+  descriptionInput.value = "";
 }
 
-// ----------------------------------------------------------------------------
-// MAIN
-// ----------------------------------------------------------------------------
+function displayTasks(tasks){
+  const display = document.querySelector(".displaySection");
+  display.innerHTML = "";
 
-let tasks = [];
+  // Create new DOM elementss
+  tasks.forEach(task => {
+    const taskItem = document.createElement('div');
+    taskItem.className = "displayItem";
+    taskItem.textContent = task.description;
+
+    // Priority color
+    if(task.priority === 1){
+      taskItem.style.backgroundColor = "red";
+    } else {
+      taskItem.style.backgroundColor = "gray";
+    }
+
+    display.appendChild(taskItem);
+  });
+}
+
+// Filter Display 
+document.getElementById('showPriorityTasks').addEventListener("click", () => {
+  const priorityTasks = taskList.filter(element => element.priority === 1);
+  displayTasks(priorityTasks);
+});
+
+document.getElementById('showNormalTasks').addEventListener("click", () => {
+  const normalTasks = taskList.filter(element => element.priority === 0);
+  displayTasks(normalTasks);
+});
+
+document.getElementById('showAll').addEventListener("click", () => {
+  displayTasks(taskList);
+});
 
 // run the function addItem when you click on the button
 let addButton = document.getElementById("addButton");
